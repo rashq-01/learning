17e1102468898afc5c374c5855b78516e4b65676
Rajesh Pandit <rashq122@gmail.com>
2024-06-13 19:41:49 +0545

    Uploading for the first time using git

---
+++ Javascript/Looping.js
@@ -0,0 +1,86 @@
+// let gameNum = 25;
+// let userNum = parseInt(prompt("Guess a number"));
+// console.log(typeof userNum);
+
+
+// while(userNum != gameNum){
+//     console.log("Guess again");
+//     userNum = prompt("Guess again");
+// }
+// console.log("You guessed it right");
+
+
+
+
+
+// let name ="Rajesh Pandit";
+// for(let x in name){
+//     console.log(x);
+// }
+
+
+
+
+
+
+
+// let name1 = "Rajesh Pandit";
+// for(let x of name1){
+//     console.log(x);
+// }
+
+
+// let num = parseInt(prompt("Guess a number"));
+// var sum = 0;
+// document.getElementById('temp').innerHTML =pandit;
+
+// function guess() {
+//     for(let i = num; i >= 1; i--) {
+//         sum = sum + i;
+//         console.log(sum);
+//     }
+//     console.log("The total sum is: ",sum);
+//     document.getElementById("temp").innerHTML = sum;
+
+// }
+
+
+// var sum = 0;
+// var child = document.createElement("div");
+// document.addEventListener("click", function(){
+//     console.log("Clicked on");
+//     document.getElementById("temp").innerHTML = document.getElementById("temp").innerHTML + "<div>"+ sum + "</div>" ;
+//     // let conf = confirm("Are you sure");
+//     // console.log(conf);
+//     sum = sum + 2;
+// })
+
+
+
+
+
+// function add(number1,number2){
+//     let number3;
+//     number3 = number1 + number2;
+//     // byd default its a number value
+//     console.log(typeof number3);
+//     console.log(number3);
+
+
+//     // conversion of number into a string
+//     let number4 = String(number3);
+//     console.log(typeof number4);
+//     console.log(number4);
+
+//     // Conversion of string and number into a boolean value
+//     let number5 = Boolean(number4);
+//     console.log(typeof number5);
+//     console.log(number5);
+
+
+//     let number6 = Boolean(number3);
+//     console.log(typeof number6);
+// }
+// add(parseInt(prompt("Enter a number")), parseInt(prompt("Enter a number")));
+
+
---
+++ React/first_react_app/src/Header.jsx
@@ -0,0 +1,9 @@
+function Header(){
+    return(
+        <div className="main-container">
+            <h1>Hello World</h1>
+            <p>This is my first react app</p>
+        </div>
+    )
+}
+export default Header;
